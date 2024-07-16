36238f572a4172161bf2aff6c3222e873c8a1288
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-07-16 17:04:28 +0300

    create ColorManager class.

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorManager {
+  const ColorManager._();
+  static const Color darkGreen = Color(0xff01301C);
+  static const Color lightGreen = Color(0xff00EA86);
+  static const Color black = Color(0xff000000);
+  static const Color white = Color(0xffffffff);
+  static const Color grey = Color(0xff444444);
+}
