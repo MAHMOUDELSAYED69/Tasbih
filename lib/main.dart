2b985afa7cfe42be4ecd869d0fb7feba18374540
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-07-17 13:26:28 +0300

    create CacheData class

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'app.dart';
+import 'utils/cache/cache.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await CacheData.cacheDataInit();
   runApp(const MyApp());
 }
